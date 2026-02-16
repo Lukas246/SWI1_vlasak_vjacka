9f6bce7efc927495e8b30be555f8c6565990e864
Lukas <vlask.lukas9@gmail.com>
2026-02-16 15:03:57 +0100

    Vytvoření entities, repositories... testovací User se zapíše do databáze přes TestController

--- backend/src/main/java/cz/osu/backend/controllers/TestController.java
+++ backend/src/main/java/cz/osu/backend/controllers/TestController.java
@@ -1,5 +1,8 @@
 package cz.osu.backend.controllers;
 
+import cz.osu.backend.entities.User;
+import cz.osu.backend.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,8 +11,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api") // Všechny cesty v tomto kontroleru budou začínat na /api
 public class TestController {
 
-    @GetMapping("/hello") // Tahle metoda se spustí při volání localhost:8080/api/hello
+    @Autowired
+    private UserRepository userRepository;
+
+    @GetMapping("/hello")
     public String sayHello() {
         return "Ahoj z backendu! Spojeni funguje.";
+    }
+
+    @GetMapping("/create-user")
+    public String createUser() {
+        User user = new User("johndoe", "john@example.com", "password123", "John", "Doe");
+        userRepository.save(user);
+        return "Uživatel John Doe byl úspěšně vytvořen a uložen do databáze.";
     }
 }
