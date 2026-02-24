0653c088862378dcc59bdccbdd600b14b1698273
jakub <jakubvjacka@seznam.cz>
2026-02-24 18:39:45 +0100

    Important split: removing the frontend and creating a new repository with the frontend

--- backend/src/main/java/cz/vlasak_vjacka/backend/InstrumentRepository.java
+++
@@ -1,9 +0,0 @@
-package cz.vlasak_vjacka.backend;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface InstrumentRepository extends JpaRepository<Instrument, Long> {
-
-}
